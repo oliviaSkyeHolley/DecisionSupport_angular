370ac65767824e8001c0641f79762e9ef0f2d22c
oliviaSkyeHolley <o.holley.aus@gmail.com>
2024-09-24 15:05:23 +1000

    setting the theming for the refactored code

--- src/app/_components/menu/menu.component.ts
+++ src/app/_components/menu/menu.component.ts
@@ -4,7 +4,7 @@
  * @description
  *  Provides navigation between the following pages: login/out, process creator, decision support.
  */
-
+/*
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -16,5 +16,26 @@ import { RouterModule } from '@angular/router';
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+
+} */
+
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {AuthService} from "../../_services/auth.service";
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [RouterModule, NgIf, NgOptimizedImage],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+  constructor(protected authService: AuthService, private router: Router) {}
+  logout(): void {
+    this.authService.logout();
+  }
+
 
 }
