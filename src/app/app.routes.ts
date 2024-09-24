fd82c62fb92db513a7b83cfa63ad562870849533
harryrowlands <rowlands.h02@gmail.com>
2024-09-24 12:26:33 +1000

    SideNav and Investigation Carrying Out.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 import {AuthenticateComponent} from './_components/authenticate/authenticate.component';
 import {InvestigationListComponent} from './_components/investigation-list/investigation-list.component';
 import {AuthGuard} from './_services/auth.guard';
+import { InvestigationComponent } from './_components/investigation/investigation.component';
 
 export const appRoutes: Routes = [
     { path: 'login', component: AuthenticateComponent},
@@ -9,6 +10,6 @@ export const appRoutes: Routes = [
     //{ path: 'process/:id', canActivate: [AuthGuard]},
     //{ path: 'process/:id/update', canActivate: [AuthGuard]},
     { path: 'support', component: InvestigationListComponent, canActivate: [AuthGuard]},
-    //{ path: 'support/:id', component: InvestigationListComponent, canActivate: [AuthGuard]},
+    { path: 'support/:id', component: InvestigationComponent, canActivate: [AuthGuard]},
     { path: '**', redirectTo: 'login' }
 ];
