9cd1607d1ddfc86e0c422e3338ac593888c8b7f9
oliviaSkyeHolley <o.holley.aus@gmail.com>
2024-09-25 12:45:25 +1000

    Updated the look of the site

--- src/app/_components/home/home.component.spec.ts
+++ src/app/_components/home/home.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
-
+//for the home page
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
