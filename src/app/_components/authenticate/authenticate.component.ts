d9204d92f5810a4a6b3c8cdb74588ddfb5b17dd0
oliviaSkyeHolley <o.holley.aus@gmail.com>
2024-09-27 00:11:35 +1000

    updating style

--- src/app/_components/authenticate/authenticate.component.ts
+++ src/app/_components/authenticate/authenticate.component.ts
@@ -8,16 +8,17 @@
 import {Component} from '@angular/core';
 import {Router, RouterModule} from '@angular/router';
 import {AuthService} from '../../_services/auth.service';
-import {CommonModule} from "@angular/common";
+import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {MatFormField} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 
+//
 @Component({
   selector: 'app-authenticate',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule, MatFormField, MatInputModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, FormsModule, MatFormField, MatInputModule, MatButtonModule, NgOptimizedImage],
   templateUrl: './authenticate.component.html',
   styleUrl: './authenticate.component.scss'
 })
@@ -32,7 +33,7 @@ export class AuthenticateComponent {
     this.authService.login(this.username, this.password).subscribe({
       next: (res) => {
         this.authService.setTokens(res.access_token, res.refresh_token);
-        this.router.navigate(['/support']); // Where do you want to be sent after successful login?
+        this.router.navigate(['/home']);
       },
       error: (err) => {
         console.error('Login error', err);
@@ -41,7 +42,4 @@ export class AuthenticateComponent {
     });
   }
 
-  logout(): void {
-    this.authService.logout();
-  }
 }
