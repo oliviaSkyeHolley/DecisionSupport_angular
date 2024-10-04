b72d96fcb60fbb6b83ccc7f372689e21c54e77a4
oliviaSkyeHolley <o.holley.aus@gmail.com>
2024-10-04 20:26:01 +1000

    updated the environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,30 +2,30 @@ export const environment = {
   production: false,
 
   //Authentication Endpoints
-  apiUrl: 'http://complaintdrupal.local/oauth/token',
-  csrfTokenUrl: 'http://complaintdrupal.local/session/token',
+  apiUrl: 'http://decisiondrupal.local/oauth/token',
+  csrfTokenUrl: 'http://decisiondrupal.local/session/token',
 
   //Process Endpoints
-  getProcessURL: 'http://complaintdrupal.local/rest/process/get/',
-  getProcessListURL: 'http://complaintdrupal.local/rest/process/list',
-  postProcessURL: 'http://complaintdrupal.local/rest/process/post',
-  duplicateProcessURL: 'http://complaintdrupal.local/rest/process/duplicate',
-  patchProcessURL: 'http://complaintdrupal.local/rest/process/patch/',
-  updateProcessURL:'http://complaintdrupal.local/rest/process/update/',
-  archiveProcessURL: 'http://complaintdrupal.local/rest/process/delete/',
+  getProcessURL: 'http://decisiondrupal.local/rest/process/get/',
+  getProcessListURL: 'http://decisiondrupal.local/rest/process/list',
+  postProcessURL: 'http://decisiondrupal.local/rest/process/post',
+  duplicateProcessURL: 'http://decisiondrupal.local/rest/process/duplicate',
+  patchProcessURL: 'http://decisiondrupal.local/rest/process/patch/',
+  updateProcessURL:'http://decisiondrupal.local/rest/process/update/',
+  archiveProcessURL: 'http://decisiondrupal.local/rest/process/delete/',
 
   //Investigation Endpoints
-  getInvestigationURL: 'http://complaintdrupal.local/rest/investigation/get/',
-  getInvestigationListURL: 'http://complaintdrupal.local/rest/investigation/list',
-  postInvestigationURL: 'http://complaintdrupal.local/rest/investigation/post',
-  patchInvestigationURL: 'http://complaintdrupal.local/rest/investigation/update/',
-  archiveInvestigationURL: 'http://complaintdrupal.local/rest/investigation/delete/',
+  getInvestigationURL: 'http://decisiondrupal.local/rest/investigation/get/',
+  getInvestigationListURL: 'http://decisiondrupal.local/rest/investigation/list',
+  postInvestigationURL: 'http://decisiondrupal.local/rest/investigation/post',
+  patchInvestigationURL: 'http://decisiondrupal.local/rest/investigation/update/',
+  archiveInvestigationURL: 'http://decisiondrupal.local/rest/investigation/delete/',
 
   //Document Upload Endpoints
-  fileUploadURL: 'http://complaintdrupal.local/file/upload/investigation_documents/_/file',
-  postInvestigationDocumentsURL: 'http://complaintdrupal.local/rest/investigation/document/post',
-  getInvestigationDocumentsURL: 'http://complaintdrupal.local/rest/investigation/document/get/',
-  archiveInvestigationDocumentsURL: 'http://complaintdrupal.local/rest/investigation/document/delete/',
+  fileUploadURL: 'http://decisiondrupal.local/file/upload/investigation_documents/_/file',
+  postInvestigationDocumentsURL: 'http://decisiondrupal.local/rest/investigation/document/post',
+  getInvestigationDocumentsURL: 'http://decisiondrupal.local/rest/investigation/document/get/',
+  archiveInvestigationDocumentsURL: 'http://decisiondrupal.local/rest/investigation/document/delete/',
 
   //Client
   clientId: 'your-client-id',
