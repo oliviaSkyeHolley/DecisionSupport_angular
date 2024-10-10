1d070358e8fa25320cf97deefa947137e0a4988e
Ruban111 <129514333+Ruban111@users.noreply.github.com>
2024-10-10 21:55:38 +1100

    User Role

--- src/app/_services/auth.guard.ts
+++ src/app/_services/auth.guard.ts
@@ -7,7 +7,7 @@
 */
 
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -17,12 +17,24 @@ export class AuthGuard implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.authService.isLoggedIn()) {
-      return true;
-    } else {
-      this.router.navigate(['/user/login']);
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+
+    const userRole = this.authService.getUserRole();
+  
+  if (this.authService.isLoggedIn()) {
+    // Check if user is an admin
+    if (userRole?.includes('administrator')) {
+      return true; 
+    }
+    // Check if user is a regular user and restrict access to the process page
+    if (userRole?.includes('user') && (route.routeConfig?.path === 'process' || route.routeConfig?.path === 'process/:id')) {
+      this.router.navigate(['/home']); 
       return false;
     }
+    return true; 
+  } else {
+    this.router.navigate(['/user/login']); // Redirect to login if not authenticated
+    return false;
+  }
   }
 }
